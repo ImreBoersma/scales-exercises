b291c683166dd04eab2b92de01ca5c2cea1588b3
Imre Boersma <imre.boersma@gmail.com>
2022-09-23 09:06:44 +0200

    add react import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { fetchScales, selectScales } from './store/scalesSlice';
 import { useAppDispatch, useAppSelector } from './utils/hooks';
